93e170409884fe282c2f7ae642118d40632e41ba
UnEvolvedGod <50989656+UnEvolvedGod@users.noreply.github.com>
2024-12-14 13:16:39 -0500

    fixint some stuff

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/world.tscn")
-
+	Engine.time_scale = 1
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/options_menu.tscn")
 
