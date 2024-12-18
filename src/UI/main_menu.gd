5f07a9d6d92e1211341c3e3428c3131da54e9a8e
UnEvolvedGod <50989656+UnEvolvedGod@users.noreply.github.com>
2024-12-17 23:56:13 -0500

    Volume Slider Working

--- src/UI/main_menu.gd
+++ src/UI/main_menu.gd
@@ -1,4 +1,3 @@
-class_name MainMenu
 extends Control
 
 ## Main Menu
--- src/UI/options.gd
+++ src/UI/options.gd
@@ -5,12 +5,14 @@ extends Control
 ##
 ## Buttons speak for themselves
 
+@onready var main_menu_music = "res://Music/main_menu_music.tscn"
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
 
 
 func _on_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, value)
+	MainMenuMusic.change_volume(value)
 
 
 func _on_resolutions_item_selected(index: int) -> void:
