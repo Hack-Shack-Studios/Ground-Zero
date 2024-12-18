3d7c403080a0f26f3100f1d830b46723c9683329
UnEvolvedGod <50989656+UnEvolvedGod@users.noreply.github.com>
2024-12-17 19:04:23 -0500

    Main Menu Complete Except Credits

--- src/UI/main_menu.gd
+++ src/UI/main_menu.gd
@@ -5,15 +5,18 @@ extends Control
 ##
 ## Handles Play, Options, and Quit buttons. The buttons explain themselves.
 
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/Pre-Alpha/pre_alpha.tscn")
-	Engine.time_scale = 1
-
-
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/options_menu.tscn")
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/Pre-Alpha/pre_alpha.tscn")
+	Engine.time_scale = 1
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
